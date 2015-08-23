7420c9be56e33897aface96c39c089a24513620c
Dylan Morshead <dylanmorshead@gmail.com>
2015-08-23 00:18:05 -0700

    Create length_banner.py

---
+++ length_banner.py
@@ -0,0 +1,12 @@
+# Variable Length Banner
+
+def banner(string):
+    display = "* {0} *".format(string)
+    length = len(display)
+    print("*" * length)
+    print(display)
+    print("*" * length)
+    
+
+banner("10034324432")
+banner("Dylan Morshead")
